af78175701a709e2235b275c1793ee0af636ef09
haiyang <qhy020927@gmail.com>
2024-04-27 10:30:23 +0000

    correct port

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
 
 
 // 设置端口并启动服务器
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
