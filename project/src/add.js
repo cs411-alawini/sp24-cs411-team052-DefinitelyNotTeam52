852086b28538ffbff235190027121176a933a030
Aoyang <aoyangl2@illinois.edu>
2024-05-01 02:30:09 +0000

    kj

---
+++ project/src/add.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AddPage() {
+  console.log("Rendering AddPage");
+  
+  return (
+    <div style={{ marginTop: '500px', textAlign: 'center' }}>
+      <h1>Add Page</h1>
+      <p>This is the page where you can add new items.</p >
+    </div>
+  );
+}
+
+export default AddPage;
---
+++ project/src/modify.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ModifyPage() {
+  console.log("Rendering modify");
+
+  return (
+    <div>
+      <h1>Modify Page</h1>
+      <p>This is the page where you can modify existing items.</p >
+    </div>
+  );
+}
+
+export default ModifyPage;
